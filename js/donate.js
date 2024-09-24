97e75b49ac2e9c7b969f1c0fbacf6f6d6a673e36
nithilanoor <nithilanoorchowdhury@gmail.com>
2024-09-25 01:46:30 +0600

    third commit

---
+++ js/donate.js
@@ -0,0 +1,96 @@
+document.getElementById('btn-donate').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addDonate = document.getElementById('input-donate').value;
+
+    // console.log(addDonate);
+
+    if(isNaN(addDonate) || addDonate < 0){
+        alert('Invalid Donation amount.');
+        return;
+    }
+
+    if(addDonate !== isNaN){
+
+        const balance = document.getElementById('donated-amount').innerText;
+        console.log(balance);
+
+        const addDonateNumber = parseFloat(addDonate);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addDonateNumber + balanceNumber;
+        
+        console.log(newBalance);
+
+        document.getElementById('donated-amount').innerText = newBalance;
+    }
+    else{
+        alert('Invalid Donation amount.')
+    }
+
+})
+
+
+
+document.getElementById('btn-donate-two').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addDonate = document.getElementById('input-donate-two').value;
+
+    // console.log(addDonate);
+
+    if(isNaN(addDonate) || addDonate < 0){
+        alert('Invalid Donation amount.');
+        return;
+    }
+
+    if(addDonate !== isNaN){
+
+        const balance = document.getElementById('donated-amount-two').innerText;
+        console.log(balance);
+
+        const addDonateNumber = parseFloat(addDonate);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addDonateNumber + balanceNumber;
+        
+        console.log(newBalance);
+
+        document.getElementById('donated-amount-two').innerText = newBalance;
+    }
+    else{
+        alert('Invalid Donation amount.')
+    }
+
+})
+
+
+
+document.getElementById('btn-donate-three').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addDonate = document.getElementById('input-donate-three').value;
+
+    // console.log(addDonate);
+
+    if(isNaN(addDonate) || addDonate < 0){
+        alert('Invalid Donation amount.');
+        return;
+    }
+
+    if(addDonate !== isNaN){
+
+        const balance = document.getElementById('donated-amount-three').innerText;
+        console.log(balance);
+
+        const addDonateNumber = parseFloat(addDonate);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addDonateNumber + balanceNumber;
+        
+        console.log(newBalance);
+
+        document.getElementById('donated-amount-three').innerText = newBalance;
+    }
+    else{
+        alert('Invalid Donation amount.')
+    }
+
+})
